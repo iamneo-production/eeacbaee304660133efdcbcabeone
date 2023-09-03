44f07017c1fea6e331a26565d1a9a1b72d580208
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:36:54 +0000

    Backup Commit

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-const express = require('express');
-const app = express();
-const port = 8081;
+let currentPlayer = 'X';
+let gameOver = false;
 
-//app.use('/static', express.static('public'))
-app.use(express.static('public'))
-app.listen(port, () => console.log(`listening on port ${port}!`));
+const cells = document.querySelectorAll('.cell');
+const status = document.getElementById('status');
+const resetButton = document.getElementById('reset');
+
+// Function to check for a win
+function checkWin() {
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let combo of winCombos) {
+        const [a, b, c] = combo;
+        if (cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent) {
+            gameOver = true;
+            status.textContent = `Player ${currentPlayer} wins!`;
+        }
+    }
+
+    if (!cells.some(cell => cell.textContent === '') && !gameOver) {
+        gameOver = true;
+        status.textContent = "It's a draw!";
+    }
+    
